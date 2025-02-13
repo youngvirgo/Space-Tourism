857fa75c10f9e46b1ad6d35184d9c1ac208ffd88
Egwuatu Emmanuel <egwuatuemmanuel56@gmail.com>
2025-02-13 22:28:29 +0100

    second commit

--- tour.js
+++ tour.js
@@ -1,8 +1,8 @@
 document.querySelector(".ham").addEventListener("click",function () {
-    document.querySelector(".navbar2").style.display = "block";
+    document.querySelector(".dropdownContent").style.display = "block";
     document.querySelector(".ham").style.display = "none";
 })
-document.querySelector(".ex").addEventListener("click",function () {
-    document.querySelector(".navbar2").style.display = "none";
+document.querySelector(".close").addEventListener("click",function () {
+    document.querySelector(".dropdownContent").style.display = "none";
     document.querySelector(".ham").style.display = "block";
 })
